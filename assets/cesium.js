0a81673ccf97dc738342503bc4b1e4396f555b00
carl <carl@surflux.studio>
2023-06-16 16:02:27 +0900

    change to dart ui and adjust timing

--- assets/cesium.js
+++ assets/cesium.js
@@ -1,57 +1,32 @@
-// Create a new CesiumJS application.
-var viewer = new Cesium.Viewer('cesiumContainer', {
-    baseLayerPicker: false,
-    baseLayer: new Cesium.ImageryLayer(new Cesium.OpenStreetMapImageryProvider({
-        url: 'https://tile.surflux.studio/hot/'
-    })),
-});
-// remove toolbar
-viewer.animation.destroy()
-viewer.homeButton.destroy()
-viewer.navigationHelpButton.destroy()
-viewer.clockViewModel.destroy()
-viewer.creditDisplay.destroy()
-viewer.fullscreenButton.destroy()
-viewer.geocoder.destroy()
-viewer.infoBox.destroy()
-viewer.sceneModePicker.destroy()
-viewer.timeline.destroy()
+setTimeout(() => {
 
-// set zoom level
-viewer.scene.screenSpaceCameraController.maximumZoomDistance = 3000;
-viewer.scene.screenSpaceCameraController.minimumZoomDistance = 100;
+    // Create a new CesiumJS application.
+    var viewer = new Cesium.Viewer('cesiumContainer', {
+        baseLayerPicker: false,
+        baseLayer: new Cesium.ImageryLayer(new Cesium.OpenStreetMapImageryProvider({
+            url: 'https://tile.surflux.studio/hot/'
+        })),
+    });
+    // remove toolbar
+    viewer.animation.destroy()
+    viewer.homeButton.destroy()
+    viewer.navigationHelpButton.destroy()
+    viewer.clockViewModel.destroy()
+    viewer.creditDisplay.destroy()
+    viewer.fullscreenButton.destroy()
+    viewer.geocoder.destroy()
+    viewer.infoBox.destroy()
+    viewer.sceneModePicker.destroy()
+    viewer.timeline.destroy()
 
-// set default camera
-// 사무실 126.8257072, 37.5678755
-let pointer = viewer.entities.add({
-    position: Cesium.Cartesian3.fromDegrees(126.8257072, 37.5678755),
-    label: {
-        text: "*",
-        scale: 1,
-        pixelOffset: new Cesium.Cartesian2(0, 0),
-        font: "32px Helvetica",
-        fillColor: Cesium.Color.YELLOW,
-        outlineColor: Cesium.Color.BLACK,
-        outlineWidth: 2,
-        style: Cesium.LabelStyle.FILL_AND_OUTLINE,
-    }
-});
+    // set zoom level
+    viewer.scene.screenSpaceCameraController.maximumZoomDistance = 3000;
+    viewer.scene.screenSpaceCameraController.minimumZoomDistance = 100;
 
-viewer.scene.camera.setView({
-    destination : Cesium.Cartesian3.fromDegrees(126.8257072, 37.5678755, 2000)
-});
-
-// change current position
-function showPosition(position) {
-    alert('show me position')
-    const longitude = position.coords.longitude;
-    const latitude = position.coords.latitude;
-    alert(`lon : ${longitude}, lat: ${latitude}`)
-
-    viewer.entities.remove(pointer);
-
-    pointer = viewer.entities.add({
-        position: Cesium.Cartesian3.fromDegrees(longitude, latitude),
+    // set default camera
+    // 사무실 126.8257072, 37.5678755
+    let pointer = viewer.entities.add({
+        position: Cesium.Cartesian3.fromDegrees(126.8257072, 37.5678755),
         label: {
             text: "*",
             scale: 1,
@@ -64,32 +39,59 @@ function showPosition(position) {
         }
     });
 
-    // 현재 높이
-    var cameraPosition = viewer.scene.camera.positionWC;
-    var ellipsoidPosition = viewer.scene.globe.ellipsoid.scaleToGeodeticSurface(cameraPosition);
-    var height = Cesium.Cartesian3.magnitude(Cesium.Cartesian3.subtract(cameraPosition, ellipsoidPosition, new Cesium.Cartesian3()));
-
     viewer.scene.camera.setView({
-        destination : Cesium.Cartesian3.fromDegrees(longitude, latitude, height)
+        destination : Cesium.Cartesian3.fromDegrees(126.8257072, 37.5678755, 2000)
     });
-}
 
-function err(err) {
-    alert('err! : ', err)
-    console.log('err! : ', err)
-}
+    // change current position
+    function showPosition(position) {
+        alert('show me position')
+        const longitude = position.coords.longitude;
+        const latitude = position.coords.latitude;
 
-const options = {
-    enableHighAccuracy: true,
-    timeout: 30000,
-    maximumAge: 0,
-};
+        viewer.entities.remove(pointer);
 
-if (navigator.geolocation) {
-    
-    // ios simulator 에서는 Features > Location 에 None 이 아닌 위치가 잡혀야 나온다
-    navigator.geolocation.watchPosition(showPosition, err, options);
-} else {
-    alert("Geolocation is not supported by this browser.");
-    console.log("Geolocation is not supported by this browser.");
-}
+        pointer = viewer.entities.add({
+            position: Cesium.Cartesian3.fromDegrees(longitude, latitude),
+            label: {
+                text: "*",
+                scale: 1,
+                pixelOffset: new Cesium.Cartesian2(0, 0),
+                font: "32px Helvetica",
+                fillColor: Cesium.Color.YELLOW,
+                outlineColor: Cesium.Color.BLACK,
+                outlineWidth: 2,
+                style: Cesium.LabelStyle.FILL_AND_OUTLINE,
+            }
+        });
+
+        // 현재 높이
+        var cameraPosition = viewer.scene.camera.positionWC;
+        var ellipsoidPosition = viewer.scene.globe.ellipsoid.scaleToGeodeticSurface(cameraPosition);
+        var height = Cesium.Cartesian3.magnitude(Cesium.Cartesian3.subtract(cameraPosition, ellipsoidPosition, new Cesium.Cartesian3()));
+
+        viewer.scene.camera.setView({
+            destination : Cesium.Cartesian3.fromDegrees(longitude, latitude, height)
+        });
+    }
+
+    function err(err) {
+        alert('err! : ', err)
+        console.log('err! : ', err)
+    }
+
+    const options = {
+        enableHighAccuracy: true,
+        timeout: 30000,
+        maximumAge: 0,
+    };
+
+    if (navigator.geolocation) {
+        // ios simulator 에서는 Features > Location 에 None 이 아닌 위치가 잡혀야 나온다
+        navigator.geolocation.watchPosition(showPosition, err, options);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+        // console.log("Geolocation is not supported by this browser.");
+    }
+
+}, 2000)
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,14 +2,14 @@
 const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
-const RESOURCES = {
-  "version.json": "3ecc3b5409a9c6acc66ca059d59993c5",
+
+const RESOURCES = {"version.json": "3ecc3b5409a9c6acc66ca059d59993c5",
 "gps_log_favicon.png": "5b3ee28e109ea2807ef482c87e69f41c",
-"index.html": "930fb7faed66edc1dd06532c34ac1278",
-"/": "930fb7faed66edc1dd06532c34ac1278",
+"index.html": "80f7deb0afe01cf0c07a59fd67f35e00",
+"/": "80f7deb0afe01cf0c07a59fd67f35e00",
 "gps_log_icon.png": "2435bcd0d873856dbb9956227c89b2b9",
-"main.dart.js": "0d58105672ad705eef68c6efb3f5149e",
-"flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
+"main.dart.js": "f450e4f0278efb6991274cb6903c9718",
+"flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/gps_log_icon_192.png": "8d10623f836f256370055836ca01a2aa",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -18,26 +18,52 @@ const RESOURCES = {
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "87f343151d33761f756e9cc312908832",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/cesium.js": "373bebdf4fcb07820ef748714f0e099e",
+"assets/AssetManifest.json": "6c8163faf3169c10bbd5058541ff5721",
+"assets/cesium.js": "97d8c026063dac89d14f3d4abaeaedbe",
 "assets/main.css": "41f4e9f5321fd5cebf12220d1cf6c318",
-"assets/NOTICES": "259068529bed6e57beac74e9760f7147",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/fonts/MaterialIcons-Regular.otf": "e7069dfd19b331be16bed984668fe080",
-"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
+"assets/NOTICES": "6a753fc6eb7bb0eb46cef2b9f55174d3",
+"assets/FontManifest.json": "71a4a82de411f155107da3f8dac64ebd",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_AMS-Regular.ttf": "657a5353a553777e270827bd1630e467",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Script-Regular.ttf": "55d2dcd4778875a53ff09320a85a5296",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Size3-Regular.ttf": "e87212c26bb86c21eb028aba2ac53ec3",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Typewriter-Regular.ttf": "87f56927f1ba726ce0591955c8b3b42d",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Caligraphic-Bold.ttf": "a9c8e437146ef63fcd6fae7cf65ca859",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_SansSerif-Bold.ttf": "ad0a28f28f736cf4c121bcb0e719b88a",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Main-Bold.ttf": "9eef86c1f9efa78ab93d41a0551948f7",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Fraktur-Regular.ttf": "dede6f2c7dad4402fa205644391b3a94",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Main-Regular.ttf": "5a5766c715ee765aa1398997643f1589",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_SansSerif-Italic.ttf": "d89b80e7bdd57d238eeaa80ed9a1013a",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Math-Italic.ttf": "a7732ecb5840a15be39e1eda377bc21d",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Main-Italic.ttf": "ac3b1882325add4f148f05db8cafd401",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Fraktur-Bold.ttf": "46b41c4de7a936d099575185a94855c4",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Size2-Regular.ttf": "959972785387fe35f7d47dbfb0385bc4",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_SansSerif-Regular.ttf": "b5f967ed9e4933f1c3165a12fe3436df",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Size1-Regular.ttf": "1e6a3368d660edc3a2fbbe72edfeaa85",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Caligraphic-Regular.ttf": "7ec92adfa4fe03eb8e9bfb60813df1fa",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Size4-Regular.ttf": "85554307b465da7eb785fd3ce52ad282",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Main-BoldItalic.ttf": "e3c361ea8d1c215805439ce0941a1c8d",
+"assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_Math-BoldItalic.ttf": "946a26954ab7fbd7ea78df07795a6cbc",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "57d849d738900cfd590e9adc7e208250",
+"assets/packages/wakelock_web/assets/no_sleep.js": "7748a45cd593f33280669b29c2c8919a",
+"assets/shaders/ink_sparkle.frag": "f8b80e740d33eb157090be4e995febdf",
+"assets/AssetManifest.bin": "501089de402c5579154ea37c536f56cc",
+"assets/fonts/MaterialIcons-Regular.otf": "62ec8220af1fb03e1c20cfa38781e17e",
+"canvaskit/skwasm.js": "1df4d741f441fa1a4d10530ced463ef8",
+"canvaskit/skwasm.wasm": "6711032e17bf49924b2b001cef0d3ea3",
+"canvaskit/chromium/canvaskit.js": "8c8392ce4a4364cbb240aa09b5652e05",
+"canvaskit/chromium/canvaskit.wasm": "fc18c3010856029414b70cae1afc5cd9",
+"canvaskit/canvaskit.js": "76f7d822f42397160c5dfc69cbc9b2de",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
-"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
-};
-
+"canvaskit/canvaskit.wasm": "f48eaf57cada79163ec6dec7929486ea",
+"canvaskit/skwasm.worker.js": "19659053a277272607529ef87acf9d8a"};
 // The application shell files that are downloaded before a service worker can
 // start.
-const CORE = [
-  "main.dart.js",
+const CORE = ["main.dart.js",
 "index.html",
 "assets/AssetManifest.json",
 "assets/FontManifest.json"];
+
 // During install, the TEMP cache is populated with the application shell files.
 self.addEventListener("install", (event) => {
   self.skipWaiting();
@@ -48,7 +74,6 @@ self.addEventListener("install", (event) => {
     })
   );
 });
-
 // During activate, the cache is populated with the temp files downloaded in
 // install. If this service worker is upgrading from one with a saved
 // MANIFEST, then use this to retain unchanged resource files.
@@ -70,6 +95,8 @@ self.addEventListener("activate", function(event) {
         await caches.delete(TEMP);
         // Save the manifest to make future upgrades efficient.
         await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
         return;
       }
       var oldManifest = await manifest.json();
@@ -95,6 +122,8 @@ self.addEventListener("activate", function(event) {
       await caches.delete(TEMP);
       // Save the manifest to make future upgrades efficient.
       await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
       return;
     } catch (err) {
       // On an unhandled exception the state of the cache cannot be guaranteed.
@@ -105,7 +134,6 @@ self.addEventListener("activate", function(event) {
     }
   }());
 });
-
 // The fetch handler redirects requests for RESOURCE files to the service
 // worker cache.
 self.addEventListener("fetch", (event) => {
@@ -145,7 +173,6 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
-
 self.addEventListener('message', (event) => {
   // SkipWaiting can be used to immediately activate a waiting service worker.
   // This will also require a page refresh triggered by the main worker.
@@ -158,7 +185,6 @@ self.addEventListener('message', (event) => {
     return;
   }
 });
-
 // Download offline will check the RESOURCES for all files not in the cache
 // and populate them.
 async function downloadOffline() {
@@ -179,7 +205,6 @@ async function downloadOffline() {
   }
   return contentCache.addAll(resources);
 }
-
 // Attempt to download the resource online before falling back to
 // the offline cache.
 function onlineFirst(event) {
